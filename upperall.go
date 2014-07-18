54be08a7e7cd188bd28a54371de4e67a1009c9fb
coolwuxing <coolwuxing@gmail.com>
2014-07-18 19:50:55 -0400

    path walk

--- upperall.go
+++ upperall.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "path/filepath"
+    "os"
+    "flag"
+    "fmt"
+)
+
+func visit(path string, f os.FileInfo, err error) error {
+    fmt.Printf("%s\n", path)
+    return nil
+} 
+
 
 func main() {
-    fmt.Printf("Upper all found strings. \n")
+    flag.Parse()
+    root := flag.Arg(0)
+    err := filepath.Walk(root, visit)
+    fmt.Printf("Walk ends. err=%v\n", err)
 }
