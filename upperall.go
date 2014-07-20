465210dfc9bea1d862cccaf7951c192027a65ce0
coolwuxing <coolwuxing@gmail.com>
2014-07-20 15:39:17 -0400

    add comments

--- upperall.go
+++ upperall.go
@@ -1,3 +1,6 @@
+// Created by Xing Wu, July 17, 2014
+// upperall: Upper all the strings found in current folder and output the result.
+
 package main
 
 import (
@@ -27,17 +30,25 @@ func (a ByOccurrences) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByOccurrences) Less(i, j int) bool { return a[i].occurrences> a[j].occurrences}
 
 func visit(path string, f os.FileInfo, err error) error {
+    // only process files
     if !f.IsDir() {
         //fmt.Printf("%s\n", path)
+
+        // open file for read/write
         fi, err := os.OpenFile(path, os.O_RDWR, 0666)
         if err != nil {
             return err    
         }
+
+        // read 1024 bytes each time
         fsize := f.Size()
         buffsize := int64(1024)
         buf := make([]byte, buffsize)
         matches := 0
         for it := int64(0); it < fsize; it+=buffsize {
+
+            // To support all data files, we need to rewind (len(target)-1) to 
+            // make sure we don't miss any target string at the border 
             n := 0
             i := int64(0)
             if it == 0 {
@@ -45,10 +56,14 @@ func visit(path string, f os.FileInfo, err error) error {
             } else {
                 i = it - int64(len(target)) + 1
             }
+
+            // read to buffer
             n, err = fi.ReadAt(buf, i)
             if n > 0 {
                 s := string(buf[:n])
                 //fmt.Printf("data: %s\n", s)
+
+                // find target string and upper all of them, count the occurrences 
                 for {
                     offset := strings.Index(s, target) 
                     if offset >=0 {
@@ -60,6 +75,8 @@ func visit(path string, f os.FileInfo, err error) error {
                         break
                     }
                 }
+
+                // append matches to array 
                 if matches > 0 {
                     news := strings.Replace(string(buf[:n]), target, upper, -1)
                     _, err = fi.WriteAt([]byte(news), i) 
@@ -83,6 +100,8 @@ func main() {
     }
     upper = strings.ToUpper(target)
     _ = filepath.Walk(".", visit)
+
+    // sort by decreasing order and output
     sort.Sort(ByOccurrences(m))
     output := ""
     for i := range m {
